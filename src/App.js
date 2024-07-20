d0e3dd195c5bac7acbc0ded83a6acc97725fe496
Nhi Tran <trannhi@Trans-MacBook-Air.local>
2024-07-20 12:10:18 -0500

    New changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React. Happy Coding
+          Learn React
         </a>
       </header>
     </div>
